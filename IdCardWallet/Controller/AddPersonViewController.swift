7e35390f4888ff10b7434c9e98b6a7108accd151
Billbert1705 <billbertpohandy@icloud.com>
2023-07-25 01:28:12 +0700

    Task : Create Add Person Page

--- IdCardWallet/Controller/AddPersonViewController.swift
+++ IdCardWallet/Controller/AddPersonViewController.swift
@@ -14,17 +14,33 @@ class AddPersonViewController: UIViewController{
     @IBOutlet weak var EmailField: TextFieldID!
     @IBOutlet weak var AddressField: TextFieldID!
     @IBOutlet weak var BirthdayField: TextFieldID!
+    @IBOutlet weak var SocialMedia: UIButton!
+    @IBOutlet weak var Note: UIButton!
+    @IBOutlet weak var Reminder: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        navigationController?.navigationBar.barTintColor = .blue
-//        navigationController?.navigationBar.isTranslucent = false
-//        
+
         let appearance =  UINavigationBarAppearance()
         appearance.configureWithOpaqueBackground()
-        appearance.backgroundColor = .blue
+        appearance.backgroundColor = UIColor(red: 0.035, green: 0.173, blue: 0.298, alpha: 1)
         navigationController?.navigationBar.standardAppearance = appearance
         navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
+
+        let textAttributes: [NSAttributedString.Key: Any] = [
+                   .foregroundColor: UIColor.white,
+               ]
+               appearance.titleTextAttributes = textAttributes
+               appearance.largeTitleTextAttributes = textAttributes // For large titles
+
+               // Apply the appearance to the navigation bar
+               navigationController?.navigationBar.standardAppearance = appearance
+               navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
+
+               // Ensure the appearance is not affected by the system's appearance
+               navigationController?.navigationBar.compactAppearance = appearance
+               navigationController?.navigationBar.scrollEdgeAppearance = appearance
+               navigationController?.navigationBar.compactScrollEdgeAppearance = appearance
         
         FullnameField.setupView(placeholders: "e.g. Mark Lee", labels: "Fullname", delegates: self)
         
@@ -40,6 +56,11 @@ class AddPersonViewController: UIViewController{
         
             
     }
+  
+}
+
+protocol SocialMediaSelectionDelegate: AnyObject {
+    func didSelectSocialMedia(platform: String)
 }
 
 extension AddPersonViewController: textFieldIDDelegate{
